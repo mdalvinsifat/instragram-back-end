eaf1f030948cdc24543b53a96c65e07b6bff7d71
mdalvinsifat <mdalvinsifat@gmail.com>
2025-05-06 00:57:25 +0600

    route-add

--- route/PostRoute.js
+++ route/PostRoute.js
@@ -1,20 +1,23 @@
 
 
-const express = require("express")
-const { uploads } = require("../middlwares/multer")
-const IsAuthenticated = require("../middlwares/IsAuthenticated")
-const {addNewPost, getAllPost} = require("../Controller/postController")
+    const express = require("express")
+    const { uploads } = require("../middlwares/multer")
+    const IsAuthenticated = require("../middlwares/IsAuthenticated")
+    const {addNewPost, getAllPost, deletePost, likePost, dislikePost, addComment} = require("../Controller/postController")
 
-const router = express.Router()
-
-
-
-router.post("/addPost", IsAuthenticated, uploads.single("image"),addNewPost )
-router.get("/allpost", IsAuthenticated,getAllPost)
+    const router = express.Router()
 
 
 
+    router.post("/addPost", IsAuthenticated, uploads.single("image"),addNewPost )
+    router.get("/allpost", IsAuthenticated,getAllPost)
+    router.delete("/:id", IsAuthenticated,deletePost)
+    router.get("/:id/like", IsAuthenticated,likePost)
+    router.get("/:id/dislike", IsAuthenticated,dislikePost)
+    router.post("/:id/commant", IsAuthenticated, addComment)
 
 
 
-module.exports = router ; 
+
+
+    module.exports = router ; 
