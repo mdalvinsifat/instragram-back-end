c0aba7805294697b2b0d97afc267d3080051ec47
Alvin SIfat <118036905+mdalvinsifat@users.noreply.github.com>
2025-05-10 15:48:38 +0600

    Update UserController.js

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -75,10 +75,11 @@ exports.LoginController = async (req, res) => {
         });
 
         return res.cookie("token", token, {
-            httpOnly: true,
-            sameSite: "strict",
-            maxAge: 24 * 60 * 60 * 1000, // 1 day
-        }).json({
+  httpOnly: true,
+  secure: true, 
+  sameSite: "None", 
+  maxAge: 7 * 24 * 60 * 60 * 1000, 
+}).json({
             success: true,
             message: `Welcome back ${user.userName}`,
             user: {
